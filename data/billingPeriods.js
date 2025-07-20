d907b10e8fb73bc5ed7cb81a22a43d5dca0f8a5d
aaa <bbb>
2025-07-20 17:19:36 +0800

    i

--- data/billingPeriods.js
+++ data/billingPeriods.js
@@ -22,17 +22,17 @@ const billingPeriods = [
     },
     {
         billingPeriodDate: "2025-04-24",
-        totalBillingPaid: 480,
+        totalBillingPaid: 264.4,
         meters: [
             {meterId: "1号表", currentReading: 7918.1, peopleCount: 1},
-            {meterId: "2号表", currentReading: 9930.3, peopleCount: 2},
+            {meterId: "2号表", currentReading: 9930.3, peopleCount: 3},
             {meterId: "3号表", currentReading: 7796.9, peopleCount: 1},
             {meterId: "4号表", currentReading: 5174.9, peopleCount: 1},
         ]
     },
     {
         billingPeriodDate: "2025-03-06",
-        totalBillingPaid: 450,
+        totalBillingPaid: 295.8,
         meters: [
             {meterId: "1号表", currentReading: 7887.0, peopleCount: 1},
             {meterId: "2号表", currentReading: 9895.0, peopleCount: 2},
@@ -41,3 +41,10 @@ const billingPeriods = [
         ]
     }
 ];
+
+const meterAliasMap = {
+    "1号表": "小韩家",
+    "2号表": "羊阿姨",
+    "3号表": "成哥家",
+    "4号表": "榴莲猫主人"
+};
