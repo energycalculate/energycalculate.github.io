ec913d79cbac922946024e8d1450409b72d96ac9
aaa <bbb>
2025-07-20 15:15:26 +0800

    i

--- data/billingPeriods.js
+++ data/billingPeriods.js
@@ -3,6 +3,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-07-18",
         totalBillingPaid: 453,
+        photoUrl: "data/image/2025-07-18.jpg",
         meters: [
             {meterId: "1号表", currentReading: 6070.6, peopleCount: 1},
             {meterId: "2号表", currentReading: 8148.6, peopleCount: 1},
@@ -13,6 +14,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-06-05",
         totalBillingPaid: 499,
+        photoUrl: "data/image/2025-06-05.jpg",
         meters: [
             {meterId: "1号表", currentReading: 5337.5, peopleCount: 1},
             {meterId: "2号表", currentReading: 7995.4, peopleCount: 1},
@@ -23,6 +25,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-04-24",
         totalBillingPaid: 480,
+        photoUrl: "data/image/2025-04-24.jpg",
         meters: [
             {meterId: "1号表", currentReading: 5174.9, peopleCount: 1},
             {meterId: "2号表", currentReading: 7918.1, peopleCount: 1},
@@ -33,6 +36,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-03-06",
         totalBillingPaid: 450,
+        photoUrl: "",
         meters: [
             {meterId: "1号表", currentReading: 5128.2, peopleCount: 1},
             {meterId: "2号表", currentReading: 7887.0, peopleCount: 1},
