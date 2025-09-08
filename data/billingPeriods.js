eac4602d98f91a448100d47ebc9e042037bb7f0f
aaa <bbb>
2025-09-09 00:00:27 +0800

    i

--- data/billingPeriods.js
+++ data/billingPeriods.js
@@ -3,6 +3,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-09-08",
         totalBillingPaid: 1023.70,
+        tier: 3,
         meters: [
             {meterId: "1号表", currentReading: 8372, peopleCount: 1},
             {meterId: "2号表", currentReading: 10410, peopleCount: 2},
@@ -13,6 +14,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-08-13",
         totalBillingPaid: 683.10,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 8269, peopleCount: 1},
             {meterId: "2号表", currentReading: 10240, peopleCount: 2},
@@ -23,6 +25,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-07-18",
         totalBillingPaid: 453,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 8148.6, peopleCount: 1},
             {meterId: "2号表", currentReading: 10127.6, peopleCount: 2},
@@ -33,6 +36,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-06-05",
         totalBillingPaid: 499,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7995.4, peopleCount: 1},
             {meterId: "2号表", currentReading: 10008.2, peopleCount: 3},
@@ -43,6 +47,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-04-24",
         totalBillingPaid: 264.4,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7918.1, peopleCount: 1},
             {meterId: "2号表", currentReading: 9930.3, peopleCount: 3},
@@ -53,6 +58,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-03-30",
         totalBillingPaid: 295.8,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7887.0, peopleCount: 1},
             {meterId: "2号表", currentReading: 9895.0, peopleCount: 3},
@@ -63,6 +69,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-02-08",
         totalBillingPaid: 444.5,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7794.6, peopleCount: 1},
             {meterId: "2号表", currentReading: 9803.2, peopleCount: 3},
@@ -73,6 +80,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2025-01-14",
         totalBillingPaid: 672.2,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7759.2, peopleCount: 1},
             {meterId: "2号表", currentReading: 9773.2, peopleCount: 3},
@@ -83,6 +91,7 @@ const billingPeriods = [
     {
         billingPeriodDate: "2024-12-17",
         totalBillingPaid: 380.8,
+        tier: 1,
         meters: [
             {meterId: "1号表", currentReading: 7706, peopleCount: 1},
             {meterId: "2号表", currentReading: 9717.5, peopleCount: 3},
