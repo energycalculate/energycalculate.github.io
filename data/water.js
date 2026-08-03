e5674ebc4809bef380b5578a3d238e7cac7c9145
aaa <bbb>
2026-08-03 21:56:29 +0800

    2

--- data/water.js
+++ data/water.js
@@ -1,9 +1,29 @@
-//水费数据：新一期直接往数组【最前面】追加一个对象即可
-//每期只需填：帐单日期 billingPeriodDate、水费总额 totalBillingPaid、每户人数 peopleCount
 const waterBillingPeriods = [
     {
-        billingPeriodDate: "2026-08-03",   //示例数据，请按实际帐单修改
-        totalBillingPaid: 100,
+        billingPeriodDate: "2026-08-03",
+        totalBillingPaid: 239,
+        households: [
+            {
+                name: "小韩家",
+                peopleCount: 1
+            },
+            {
+                name: "羊阿姨",
+                peopleCount: 1.5
+            },
+            {
+                name: "成哥家",
+                peopleCount: 1
+            },
+            {
+                name: "何哥家",
+                peopleCount: 2
+            },
+        ]
+    },
+    {
+        billingPeriodDate: "2026-05-25",
+        totalBillingPaid: 238.9,
         households: [
             {
                 name: "小韩家",
@@ -19,7 +39,7 @@ const waterBillingPeriods = [
             },
             {
                 name: "何哥家",
-                peopleCount: 2
+                peopleCount: 1
             },
         ]
     },
